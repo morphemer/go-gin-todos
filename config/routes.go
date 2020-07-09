249d5cbf8ae65f9d551f5517486e4c38e5f3b8b6
joe <kinjoyuji@gmail.com>
2020-07-09 20:17:48 +0900

    controller実装

--- config/routes.go
+++ config/routes.go
@@ -2,15 +2,15 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joe41203/go-gin-todos/controllers"
 )
 
+// Routes all of routes
 func Routes() *gin.Engine {
 	r := gin.Default()
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/todos", func(c *gin.Context) {
-			c.JSON(200, gin.H{"message": "Hello"})
-		})
+		v1.GET("/todos", controllers.GetTodos)
 	}
 	return r
 }
---
+++ controllers/todo.go
@@ -0,0 +1,9 @@
+// Package controllers todos
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+// GetTodos get all todos
+func GetTodos(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Hello"})
+}
