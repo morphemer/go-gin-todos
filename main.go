a823afbd6e79ac40f3f99c008770afe6d9a23bb0
joe <kinjoyuji@gmail.com>
2020-07-13 12:48:30 +0900

    reset

--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jinzhu/gorm"
-	"github.com/joe41203/go-gin-todos/config"
-	"github.com/joe41203/go-gin-todos/models"
-	"github.com/joe41203/go-gin-todos/routes"
-)
-
-var err error
+import "fmt"
 
 func main() {
-	config.DB, err = gorm.Open("mysql", config.DbURL(config.BuildDBConfig()))
-
-	if err != nil {
-		fmt.Println("status: ", err)
-	}
-
-	defer config.DB.Close()
-
-	config.DB.AutoMigrate(&models.Todo{})
-
-	r := routes.Routes()
-	r.LoadHTMLGlob("views/*/**")
-	r.Run()
+	fmt.Println("HelloWorld")
 }
