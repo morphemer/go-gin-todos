835a2a8ac0190b738d0af2c732ab7c865845f84e
joe <kinjoyuji@gmail.com>
2020-07-13 13:44:03 +0900

    HelloWorld

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joe41203/go-gin-todos/config"
 )
 
@@ -10,4 +11,12 @@ func main() {
 	fmt.Println("HelloWorld")
 	db := config.Init()
 	defer db.Close()
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "HelloWorld"})
+	})
+
+	r.Run()
 }
