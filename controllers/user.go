492219ad29ab11eda0740b38a0d83197330e3b4b
joe <kinjoyuji@gmail.com>
2020-07-14 22:33:23 +0900

    user 一覧のAPI実装

--- controllers/user.go
+++ controllers/user.go
@@ -1,8 +1,17 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joe41203/go-gin-todos/config"
+	"github.com/joe41203/go-gin-todos/models"
+)
 
 // GetAllUsers fetch all users
 func GetAllUsers(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "HelloWorld"})
+	users := []models.User{}
+	db := config.GetDB()
+	db.Find(&users)
+	c.JSON(http.StatusOK, users)
 }
